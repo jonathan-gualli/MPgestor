7aee755ff0a9c387f69dcfe10a822fc7377b29bf
Jonathan <jonathan.gualli@epn.edu.ec>
2022-12-26 23:43:50 -0500

    Login creado, login simple sin autentificación

--- app/src/main/java/com/example/mpgestor/MainActivity.java
+++ app/src/main/java/com/example/mpgestor/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.mpgestor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etNombre, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNombre = (EditText) findViewById(R.id.txtNombre);
+        etPassword = (EditText) findViewById(R.id.txtPassword);
+    }
+
+    //Metodo para el boton de ingresar
+    public void ingresar(View view){
+        if(etNombre.getText().toString().equals("Jonathan") && etPassword.getText().toString().equals("abc123")){
+            Intent principal = new Intent(this, SegundaActivity.class);
+            startActivity(principal);
+        } else {
+            Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+        }
     }
 }
